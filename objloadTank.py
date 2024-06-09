6dce4fd9aa7679507357250031a5c4dc61c2d7be
Ana Flores <a01735115@tec.mx>
2024-06-09 03:07:53 -0600

    tanque listo, solo modificar iluminación para que se vean colores

--- objloadTank.py
+++ objloadTank.py
@@ -1,68 +1,17 @@
 import os
-import pygame
 from OpenGL.GL import *
+from PIL import Image
 
 class OBJ:
-    generate_on_init = True
-
-    @classmethod
-    def loadTexture(cls, imagefile):
-        try:
-            surf = pygame.image.load(imagefile)
-            image = pygame.image.tostring(surf, 'RGBA', 1)
-            ix, iy = surf.get_rect().size
-            texid = glGenTextures(1)
-            glBindTexture(GL_TEXTURE_2D, texid)
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, ix, iy, 0, GL_RGBA, GL_UNSIGNED_BYTE, image)
-            print(f"Texture loaded: {imagefile}")
-            return texid
-        except Exception as e:
-            print(f"Error loading texture {imagefile}: {e}")
-            return None
-
-    @classmethod
-    def loadMaterial(cls, filename):
-        contents = {}
-        mtl = None
-        dirname = os.path.dirname(filename)
-
-        try:
-            with open(filename, "r") as f:
-                for line in f:
-                    if line.startswith('#'):
-                        continue
-                    values = line.split()
-                    if not values:
-                        continue
-                    if values[0] == 'newmtl':
-                        mtl = contents[values[1]] = {}
-                        print(f"New material: {values[1]}")
-                    elif mtl is None:
-                        raise ValueError("mtl file doesn't start with newmtl stmt")
-                    elif values[0] == 'map_Kd':
-                        mtl[values[0]] = values[1]
-                        imagefile = os.path.join(dirname, mtl['map_Kd'])
-                        mtl['texture_Kd'] = cls.loadTexture(imagefile)
-                        print(f"Texture map_Kd: {values[1]}")
-                    else:
-                        mtl[values[0]] = list(map(float, values[1:]))
-            print(f"Material file {filename} loaded successfully with {len(contents)} materials.")
-            return contents
-        except Exception as e:
-            print(f"Error loading material file {filename}: {e}")
-            return {}
-
     def __init__(self, filename, swapyz=False):
         self.vertices = []
         self.normals = []
         self.texcoords = []
         self.faces = []
-        self.gl_list = 0
-        dirname = os.path.dirname(filename)
+        self.materials = {}
 
         material = None
+
         try:
             with open(filename, "r") as f:
                 for line in f:
@@ -83,11 +32,6 @@ class OBJ:
                         self.normals.append(v)
                     elif values[0] == 'vt':
                         self.texcoords.append(list(map(float, values[1:3])))
-                    elif values[0] in ('usemtl', 'usemat'):
-                        material = values[1]
-                        print(f"Using material: {material}")
-                    elif values[0] == 'mtllib':
-                        self.mtl = self.loadMaterial(os.path.join(dirname, values[1]))
                     elif values[0] == 'f':
                         face = []
                         texcoords = []
@@ -95,50 +39,107 @@ class OBJ:
                         for v in values[1:]:
                             w = v.split('/')
                             face.append(int(w[0]))
-                            if len(w) >= 2 and len(w[1]) > 0:
+                            if len(w) >= 2 and w[1]:
                                 texcoords.append(int(w[1]))
                             else:
                                 texcoords.append(0)
-                            if len(w) >= 3 and len(w[2]) > 0:
+                            if len(w) >= 3 and w[2]:
                                 norms.append(int(w[2]))
                             else:
                                 norms.append(0)
-                        self.faces.append((face, norms, texcoords, material))
-            if self.generate_on_init:
-                self.generate()
+                        self.faces.append((face, texcoords, norms, material))
+                    elif values[0] == 'usemtl':
+                        material = values[1]
+                    elif values[0] == 'mtllib':
+                        self.load_materials(os.path.join(os.path.dirname(filename), values[1]))
+
             print(f"OBJ file {filename} loaded successfully.")
         except Exception as e:
             print(f"Error loading OBJ file {filename}: {e}")
 
-    def generate(self):
         self.gl_list = glGenLists(1)
         glNewList(self.gl_list, GL_COMPILE)
-        glEnable(GL_TEXTURE_2D)
         glFrontFace(GL_CCW)
         for face in self.faces:
-            vertices, normals, texture_coords, material = face
-            if material and material in self.mtl:
-                mtl = self.mtl[material]
+            vertices, texcoords, norms, material = face
+            if material and material in self.materials:
+                mtl = self.materials[material]
                 if 'texture_Kd' in mtl:
+                    glEnable(GL_TEXTURE_2D)
                     glBindTexture(GL_TEXTURE_2D, mtl['texture_Kd'])
                 else:
-                    glColor(1.0, 1.0, 1.0)  # Default to white if no texture
+                    glDisable(GL_TEXTURE_2D)
+
+                # Set material properties
+                if 'Kd' in mtl:
+                    glColor3fv(mtl['Kd'])
+                else:
+                    glColor3f(1.0, 1.0, 1.0)  # Default to white color
+
+                if 'Ka' in mtl:
+                    glMaterialfv(GL_FRONT, GL_AMBIENT, mtl['Ka'])
+                if 'Kd' in mtl:
+                    glMaterialfv(GL_FRONT, GL_DIFFUSE, mtl['Kd'])
+                if 'Ks' in mtl:
+                    glMaterialfv(GL_FRONT, GL_SPECULAR, mtl['Ks'])
+                if 'Ns' in mtl:
+                    glMaterialfv(GL_FRONT, GL_SHININESS, mtl['Ns'])
             else:
-                glColor(1.0, 1.0, 1.0)  # Default to white if no material
+                glColor3f(1.0, 1.0, 1.0)  # Default to white color
 
             glBegin(GL_POLYGON)
             for i in range(len(vertices)):
-                if normals[i] > 0:
-                    glNormal3fv(self.normals[normals[i] - 1])
-                if texture_coords[i] > 0:
-                    glTexCoord2fv(self.texcoords[texture_coords[i] - 1])
+                if norms[i] > 0:
+                    glNormal3fv(self.normals[norms[i] - 1])
+                if texcoords[i] > 0:
+                    glTexCoord2fv(self.texcoords[texcoords[i] - 1])
                 glVertex3fv(self.vertices[vertices[i] - 1])
             glEnd()
-        glDisable(GL_TEXTURE_2D)
         glEndList()
+
+    def load_materials(self, filename):
+        current_dir = os.path.dirname(filename)
+        mtl = None
+        materials = {}
+        try:
+            with open(filename, "r") as f:
+                for line in f:
+                    if line.startswith('#'):
+                        continue
+                    values = line.split()
+                    if not values:
+                        continue
+                    if values[0] == 'newmtl':
+                        mtl = materials[values[1]] = {}
+                    elif mtl is None:
+                        continue
+                    elif values[0] == 'map_Kd':
+                        image_filename = values[1]
+                        image_path = os.path.join(current_dir, image_filename)
+                        try:
+                            image = Image.open(image_path)
+                            image = image.transpose(Image.FLIP_TOP_BOTTOM)
+                            ix, iy, image = image.size[0], image.size[1], image.tobytes()
+                            texid = glGenTextures(1)
+                            glBindTexture(GL_TEXTURE_2D, texid)
+                            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+                            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+                            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, ix, iy, 0, GL_RGB, GL_UNSIGNED_BYTE, image)
+                            mtl['texture_Kd'] = texid
+                        except Exception as e:
+                            print(f"Failed to load texture {image_path}: {e}")
+                    elif values[0] == 'Kd':
+                        mtl['Kd'] = list(map(float, values[1:4]))
+                    elif values[0] == 'Ka':
+                        mtl['Ka'] = list(map(float, values[1:4]))
+                    elif values[0] == 'Ks':
+                        mtl['Ks'] = list(map(float, values[1:4]))
+                    elif values[0] == 'Ns':
+                        mtl['Ns'] = float(values[1])
+            self.materials.update(materials)
+            print(f"Materials file {filename} loaded successfully.")
+        except Exception as e:
+            print(f"Error loading materials file {filename}: {e}")
 
     def render(self):
         glCallList(self.gl_list)
-
-    def free(self):
-        glDeleteLists([self.gl_list])
--- planoTank.py
+++ planoTank.py
@@ -2,7 +2,6 @@ import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
-from OpenGL.GLUT import *
 import math
 from objloadTank import OBJ
 
@@ -12,9 +11,11 @@ FOVY = 60.0
 ZNEAR = 0.01
 ZFAR = 900.0
 
-EYE_X = 300.0
-EYE_Y = 200.0
-EYE_Z = 300.0
+# Posición de la cámara más cercana
+EYE_X = 150.0
+EYE_Y = 100.0
+EYE_Z = 150.0
+
 CENTER_X = 0
 CENTER_Y = 0
 CENTER_Z = 0
@@ -25,7 +26,7 @@ UP_Z = 0
 DimBoard = 200
 
 theta = 0.0
-radius = 300
+radius = 10  # Ajuste del radio más cercano
 
 pygame.init()
 
@@ -52,8 +53,7 @@ def Axis():
     glLineWidth(1.0)
 
 def Init():
-    screen = pygame.display.set_mode(
-        (screen_width, screen_height), DOUBLEBUF | OPENGL)
+    screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF | OPENGL)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
     gluPerspective(FOVY, screen_width / screen_height, ZNEAR, ZFAR)
@@ -76,12 +76,17 @@ def Init():
 
     glShadeModel(GL_SMOOTH)
     glEnable(GL_NORMALIZE)
+    
+    glLightfv(GL_LIGHT0, GL_POSITION, (0, 200, 0, 0.0))
+    glLightfv(GL_LIGHT0, GL_AMBIENT, (0.5, 0.5, 0.5, 1.0))
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, (0.5, 0.5, 0.5, 1.0))
+    glLightfv(GL_LIGHT0, GL_SPECULAR, (0.5, 0.5, 0.5, 1.0))
 
     glClearColor(0.93, 0.86, 0.76, 1.0)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
     try:
-        obj = OBJ("AddTank/T80.obj", swapyz=True)
+        obj = OBJ("Sherman-Fury/T72Tank.obj", swapyz=True)
         objetos.append(obj)
         print("Object appended to list successfully.")
     except Exception as e:
@@ -91,6 +96,7 @@ def Init():
 
 def lookat():
     global EYE_X, EYE_Z, radius
+    radius = 150  # Ajusta este valor para acercar o alejar la cámara
     EYE_X = radius * (math.cos(math.radians(theta)) + math.sin(math.radians(theta)))
     EYE_Z = radius * (-math.sin(math.radians(theta)) + math.cos(math.radians(theta)))
     glLoadIdentity()
@@ -99,8 +105,8 @@ def lookat():
 def displayobj():
     glPushMatrix()
     glRotatef(-90.0, 1.0, 0.0, 0.0)
-    glTranslatef(0.0, 0.0, 15.0)
-    glScale(0.5, 0.5, 0.5)
+    glTranslatef(0.0, 0.0, 30.0)  # Ajusta estos valores según sea necesario
+    glScale(4.0, 4.0, 4.0)  # Ajusta estos valores según sea necesario para hacer el objeto más grande
     for obj in objetos:
         obj.render()
     glPopMatrix()
@@ -111,6 +117,7 @@ def display():
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
 
+    radius = 150  # Ajusta este valor para acercar o alejar la cámara
     gluLookAt(radius * math.cos(math.radians(theta)), EYE_Y, radius * math.sin(math.radians(theta)),
               CENTER_X, CENTER_Y, CENTER_Z, UP_X, UP_Y, UP_Z)
 
@@ -122,7 +129,15 @@ def main():
     global theta, radius
     done = False
     Init()
+    clock = pygame.time.Clock()
     while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    done = True
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_RIGHT]:
@@ -139,12 +154,8 @@ def main():
                 theta -= 1.0
             lookat()
 
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    done = True
-
         display()
+        clock.tick(60)  # Limita el bucle a 60 FPS
 
 if __name__ == "__main__":
     main()
